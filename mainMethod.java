2bc21925390eebf9430c2adee333211dc9633c2e
RESU SAINANDHU <sainandhunaiduresu@gmail.com>
2025-12-29 16:05:36 +0530

    Implement basic calculator with addition method

---
+++ mainMethod.java
@@ -0,0 +1,18 @@
+class calculator {
+    int a = 50;
+    int b = 40;
+
+    void add() {
+        int c;
+        c = a+b;
+        System.out.println(c);
+        
+    }
+}
+public class Main {
+public static void main(String args[]){
+
+  calculator c = new calculator();
+  c.add();
+ }
+}
