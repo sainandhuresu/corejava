ebd0a8e037bb03b03d4e5b4f76b29b181572bd93
RESU SAINANDHU <sainandhunaiduresu@gmail.com>
2026-01-03 10:15:55 +0530

    Add program to count number of elements in array

---
+++ countNumberOfElementsinArray.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String []args){
+        
+        int arr[] = {7,2,5,8,1,6,3,9,4};
+      
+        System.out.println("number of array elements is: "+arr.length);
+        
+    }
+}
