97b9a8a708f56a3c1443b60b696ae1daaccf5efb
RESU SAINANDHU <sainandhunaiduresu@gmail.com>
2026-01-03 10:12:34 +0530

    Implement sum of array elements in Java

---
+++ sumofArrayelements.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String []args){
+        
+        int arr[] = {7,2,5,8,1,6,3,9,4};
+        int sum = 0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            sum += arr[i];
+        }
+        System.out.println("enter the sum of array is: "+sum);
+        
+    }
+}
